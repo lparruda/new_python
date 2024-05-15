fa02dfe3ee030b4870f73353e5821c80abd0984a
Luciano Pereira de Arruda <lucianop27@gmail.com>
2024-05-14 21:54:10 -0300

    teste8

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from setuptools import setup, find_packages
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+import myproject
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+setup(
+    name=myproject.__name__,
+    version=myproject.__version__,
+    url='https://github.com/maet3608/minimal-python-project',
+    author='Author name',
+    author_email='author@gmail.com',
+    description='Template and example for a minimal Python project',
+    packages=find_packages(),
+    install_requires=[],  # e.g. ['numpy >= 1.11.1', 'matplotlib >= 1.5.1']
+)
